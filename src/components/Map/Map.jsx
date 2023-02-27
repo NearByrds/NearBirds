1dbf13af9941afba476a6a08340a063699f3aff2
Igor M <mporeigor@gmail.com>
2023-02-27 15:44:32 +0300

    add team components

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import Loader from "../../components/common/Loader/Loader"
+import NavBar from "../common/NavBar/NavBar";
+import Footer from "../common/Footer/Footer";
 
 const Map = () => {
   return (
-    <Loader/>
+    <div>
+      <NavBar />
+      <div>Map</div>
+      <div className="bg-gray-200 h-[1px] mx-[22px] md:mx-[82px]"></div>
+      <Footer />
+    </div>
   )
 }
 
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import NavBar from "../common/NavBar/NavBar";
+import Footer from "../common/Footer/Footer";
 
 const Team = () => {
   return (
-    <div>Team</div>
+    <div>
+      <NavBar />
+      <div>Team</div>
+      <div className="bg-gray-200 h-[1px] mx-[22px] md:mx-[82px]"></div>
+      <Footer />
+    </div>
   )
 }
 
