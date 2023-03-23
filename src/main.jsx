1dfd75142d0ca6cf29c88d16f637a7bbb5730cc3
Tasneem <tasneemsherif23@gmil.com>
2023-03-23 09:43:00 +0300

    add sentry configuration for routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import { BrowserRouter as Router,
+        useLocation, 
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes } from 'react-router-dom';
 import './index.css';
 import App from './App';
+
+Sentry.init({
+  dsn: "https://40c936400c8d4798a78be9c2b416eab6@o4504886346711040.ingest.sentry.io/4504886355755008",
+  integrations: [ new BrowserTracing({
+    routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+        React.useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes,
+    ),
+}),],
+  tracesSampleRate: 1.0,
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+       <App />
     </Router>
   </React.StrictMode>,
 );
