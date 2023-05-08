6fcd64d1b61ae191cf6a07288dac160cb33dc5e2
e2xen <max.ks1230@gmail.com>
2023-05-08 14:35:34 +0300

    change api calls

--- src/utils/FetchImage.jsx
+++ src/utils/FetchImage.jsx
@@ -11,22 +11,18 @@ const fetchImage = async (bird, setImageUrl) => {
             }
         }
 
-        const fileNameReq = await fetch(
-            `https://en.wikipedia.org/w/api.php?action=parse&prop=images&page=${searchText}&format=json&origin=*`,
+        const pageReq = await fetch(
+            `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${searchText}&srprop=snippet&format=json&origin=*`,
             options
         );
-        const fileNameResp = await fileNameReq.json();
-        const fileNames = fileNameResp.parse.images.filter(
-            item => item.toLowerCase().includes(searchText.replace(" ", "_"))
-            && item.toLowerCase().includes(".jpg")
-        );
-        const fileName = fileNames[0];
-        const res = await fetch(
-            `https://en.wikipedia.org/w/api.php?action=query&prop=imageinfo&titles=File:${fileName}&format=json&iiprop=url&origin=*`,
-            options,
-        );
-        const results = await res.json();
-        const imageLink = Object.values(results.query.pages)[0].imageinfo[0].url;
+        const pageResp = await pageReq.json();
+        const pageName = pageResp.query.search[0].title;
+        const imageReq = await fetch(
+            `https://en.wikipedia.org/w/api.php?action=query&titles=${pageName}&prop=pageimages&piprop=original&format=json&origin=*`
+        )
+        const imageResp = await imageReq.json();
+        const imageLink = Object.values(imageResp.query.pages)[0].original.source;
+
         setImageUrl(imageLink);
     } catch (error) {
         Sentry.captureMessage(error.message);
