a9b20b8b9398ee3459721e74c68912e791c6b69c
sh3b0 <ahmedshaban784@gmail.com>
2023-05-11 20:36:30 +0300

    Improve e2e tests

--- selenium/test.py
+++ selenium/test.py
@@ -1,47 +1,43 @@
-#Implementation of Selenium WebDriver with Python using PyTest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from time import sleep
+from selenium.webdriver.chrome.options import Options
+import time
+
+URL = 'http://64.226.66.76'
+TITLE = 'Near Birds'
+
+
+def init_browser():
+    options = Options()
+    options.add_argument('--headless')
+    chrome_driver = webdriver.Chrome(options=options)
+    chrome_driver.implicitly_wait(5)
+    chrome_driver.get(URL)
+    assert chrome_driver.title == TITLE
+    return chrome_driver
 
 
 def test_search_by_country():
-    
-    chrome_driver = webdriver.Chrome()
-    chrome_driver.implicitly_wait(5) # seconds
+    driver = init_browser()
+    driver.find_element(
+        by=By.XPATH,
+        value="//*[@id='root']/div/div/div[2]/main/div[1]/div[1]/select/option[47]"
+    ).click()
+    time.sleep(1)
+    assert "Egypt" in driver.find_element(
+        By.CLASS_NAME,
+        value="country-header"
+    ).text
+    driver.quit()
 
-    chrome_driver.get('http://64.226.66.76')
-    chrome_driver.maximize_window()
-    title = 'Near Birds'
-
-    sleep(2)
-    assert title == chrome_driver.title
-
-    country_selector_Egypt = chrome_driver.find_element(by=By.XPATH,value="//*[@id='root']/div/div/div[2]/main/div[1]/div[1]/select/option[47]")
-    country_selector_Egypt.click()
-    assert title == chrome_driver.title
-    sleep(2)
-    chrome_driver.quit()
 
 def test_search_by_bird():
-    chrome_driver = webdriver.Chrome()
-    chrome_driver.implicitly_wait(8) # seconds
-
-    chrome_driver.get('http://64.226.66.76')
-    chrome_driver.maximize_window()
-    title = 'Near Birds'
-
-    sleep(2)
-    assert title == chrome_driver.title
-
-    element = chrome_driver.find_element(by=By.XPATH,value= '//*[@id="simple-search"]')
-    element.send_keys("Red-breasted")
-    element.send_keys(Keys.RETURN)
+    driver = init_browser()
+    search_box = driver.find_element(value='simple-search')
+    time.sleep(1)
     search_query = "Red-breasted"
-    sleep(3)
-
-    lt_link = chrome_driver.find_element(by=By.XPATH,value="//*[@id='root']/div/div/div[2]/main/div[2]/div[1]/ul/li[1]/div/div[1]/h3")
-    assert search_query in lt_link.text  
-    sleep(2)
-    chrome_driver.quit()
-
+    search_box.send_keys(search_query)
+    time.sleep(1)
+    assert search_query in driver.find_element(
+        By.CLASS_NAME, value="specie-identity").text
+    driver.quit()
